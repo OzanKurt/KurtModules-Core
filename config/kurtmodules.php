c595ea29cc9211c71b5c151b1b724b456d920e41
Ozan Kurt <me@ozankurt.com>
2026-05-28 16:13:55 +0300

    feat(core): add base config file

---
+++ config/kurtmodules.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | KurtModules global config
+    |--------------------------------------------------------------------------
+    |
+    | Settings shared by every kurtmodules package.
+    |
+    */
+
+    'user_model' => env('KURTMODULES_USER_MODEL'),
+
+    'date_format' => 'Y-m-d H:i:s',
+];
